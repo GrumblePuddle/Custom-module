15e783a7af9c77088d237f279e2b422b178fe3a4
Gustav Liffner <gurra_liffner@hotmail.com>
2025-02-26 21:59:21 +0000

    first commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { reverseString, countCharacters } from "./stringUtils";
+
+console.log(
+  reverseString(
+    "It is important to learn Node.js in order to debug your code without using the browser."
+  )
+);
+console.log(
+  countCharacters(
+    "It is important to learn Node.js in order to debug your code without using the browser."
+  )
+);
