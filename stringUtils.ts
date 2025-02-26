43e54fc858f2e313fb5ec69f53d4379003cd29cb
Gustav Liffner <gurra_liffner@hotmail.com>
2025-02-26 22:14:40 +0000

    part 1 and 2 complete

--- stringUtils.ts
+++ stringUtils.ts
@@ -5,3 +5,7 @@ export const reverseString = (str: string): string => {
 export const countCharacters = (str: string): number => {
   return str.length;
 };
+
+export const toUpperCase = (str: string): string => {
+  return str.toUpperCase();
+};
